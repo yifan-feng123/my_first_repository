770589b9d7ff01a783575239805f509c4d2ae992
yifan-feng123 <2512302790@qq.com>
2025-08-29 09:29:13 +0800

    Update backbone.py

--- backbone.py
+++ backbone.py
@@ -1,11 +1,10 @@
-import torch
-import torch.nn as nn
+
 
 
 def autopad(k, p=None):
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k] 
-return p
+    return p
 
 class SiLU(nn.Module):  
     @staticmethod
